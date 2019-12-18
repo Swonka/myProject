4b72f1735645e27ba41864303d3cd42971ba8e9d
Swonka <36085212+Swonka@users.noreply.github.com>
2019-12-18 19:54:13 +0100

    one unit test with redux working

---
+++ src/app/myRedux.spec.ts
@@ -0,0 +1,23 @@
+import {rootReducer } from './store'
+/*
+describe('redux test', () =>{ 
+
+    it('should test the initial state', ()=>{
+
+        const  defaultState  = rootReducer;
+        const action = {type: ""};
+        const state = defaultState(undefined, action)
+
+        expect(defaultState).toBe(state);
+       
+
+
+
+    });
+
+
+
+
+})
+
+*/
